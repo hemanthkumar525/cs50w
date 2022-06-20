51dca20be3578ba8fa5cd05eb584cda5e7a1af91
luciferextr <hemanthrockzzzz199@gmail.com>
2022-06-20 22:10:18 +0530

    sub code for ai

---
+++ knowledge/harry.py
@@ -0,0 +1,13 @@
+from logic import *
+
+rain = Symbol("rain")  #it is a raining
+hagrid = Symbol("hagrid") #harry visits hagrid
+dumbledore = Symbol("dumbledore") #harry visits dumbledore
+
+knowledge = And(
+    Implication(Not(rain), hagrid),
+    Or(hagrid, dumbledore),
+    Not(And(hagrid,dumbledore)),
+    dumbledore
+)
+print(knowledge.formula())
